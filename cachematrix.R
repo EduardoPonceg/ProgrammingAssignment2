eebce1fab03bb7de5ff4289072e78bd83adfe899
EduardoPonceg <67066638+EduardoPonceg@users.noreply.github.com>
2020-08-24 22:05:07 -0500

    Update cachematrix.R
    Make the results of the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## x is a invertible matrix
+## we get an error if x is not 
 makeCacheMatrix <- function(x = matrix()) {
-
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+          }
+          get <- function() x
+          setinverse <- function(inverse) inv <<- inverse
+          getinverse <- function() inv
+          list(set = set, get = get, setinverse = setinverse,getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
+## x is the result of makeCacheMatrix function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("getting cached matrix inverse")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setinverse(inv)
+        inv
 }
